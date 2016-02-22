31b720d83688a171a7e41545965c79d9ca9344b0
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-02-21 23:09:02 -0700

    start

---
+++ darkroom/lib/js/plugins/darkroom.save.js
@@ -0,0 +1,24 @@
+(function() {
+'use strict';
+
+Darkroom.plugins['save'] = Darkroom.Plugin.extend({
+
+  defaults: {
+    callback: function() {
+      this.darkroom.selfDestroy();
+    }
+  },
+
+  initialize: function InitializeDarkroomSavePlugin() {
+    var buttonGroup = this.darkroom.toolbar.createButtonGroup();
+
+    this.destroyButton = buttonGroup.createButton({
+      icon: 'fa fa-save',
+      image: 'save'
+    });
+
+    this.destroyButton.addEventListener('click', this.options.callback.bind(this));
+  },
+});
+
+})();
